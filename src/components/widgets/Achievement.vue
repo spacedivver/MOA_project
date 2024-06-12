a0150e99887c35c6d5f31491cf53b8d4d3f43743
ming-taro <hera002008@naver.com>
2024-06-12 13:21:40 +0900

    Feat: 목표 금액 달성도 차트 구현

---
+++ src/components/widgets/Achievement.vue
@@ -0,0 +1,69 @@
+<!-- ChartComponent.vue -->
+<template>
+    <div>
+        <div id="achievement_chart"></div>
+    </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+import ApexCharts from 'apexcharts';
+
+onMounted(() => {
+    var options = {
+            series: [74],
+            chart: {
+                fontFamily: 'inherit',
+                height: 300,
+                type: 'radialBar',
+                offsetY: 0
+            },
+            plotOptions: {
+                radialBar: {
+                    startAngle: -90,
+                    endAngle: 90,
+
+                    hollow: {
+                        margin: 0,
+                        size: "70%"
+                    },
+                    dataLabels: {
+                        showOn: "always",
+                        name: {
+                            show: true,
+                            fontSize: "13px",
+                            fontWeight: "700",
+                            offsetY: -5,
+                            color: '#B6C0D1'
+                        },
+                        value: {
+                            color: '#4B5675',
+                            fontSize: "30px",
+                            fontWeight: "700",
+                            offsetY: -40,
+                            show: true
+                        }
+                    },
+                    track: {
+                        background: '#87D4F9',
+                        strokeWidth: '100%'
+                    }
+                }
+            },
+            // progress color
+            colors: ['#24B0FF'],
+            stroke: {
+                lineCap: "round",
+            },
+            labels: ["My Achievements"]
+        };
+
+    new ApexCharts(document.querySelector("#achievement_chart"), options).render();
+});
+</script>
+
+<style scoped>
+#achievement_chart {
+    width: 100%;
+}
+</style>
