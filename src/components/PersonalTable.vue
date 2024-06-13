fca7f43e30b317590b934d7849d7199ac55c8356
DaehoGo <rualneox@dgu.ac.kr>
2024-06-14 02:49:57 +0900

    fix: 거래 내역 테이블 수정

--- src/components/PersonalTable.vue
+++ src/components/PersonalTable.vue
@@ -1,31 +1,24 @@
 <template>
     <tbody class="fw-bold text-gray-600">
-        <tr v-for="item in data" :key="item.id">
-            <td>{{ item.date }}</td>
-            <td class="text-end">{{ item.amount }}</td>
-            <td class="text-end">{{ item.type }}</td>
-            <td class="text-end">{{ item.category }}</td>
-            <td class="text-end">{{ item.memo }}</td>
-        </tr>
+      <tr v-for="item in data" :key="item.id">
+        <td>{{ item.date }}</td>
+        <td class="text-end">{{ item.amount }}</td>
+        <td class="text-end">
+            <span v-if="item.type === '수입'" class="badge py-3 px-4 fs-7 badge-light-primary">수입</span>
+            <span v-else-if="item.type === '지출'" class="badge py-3 px-4 fs-7 badge-light-danger">지출</span>
+            <span v-else>{{ item.type }}</span>
+        </td>
+        <td class="text-end">{{ item.category }}</td>
+        <td class="text-end">{{ item.memo }}</td>
+      </tr>
     </tbody>
-</template>
-
-<script setup>
-import { ref, onMounted } from 'vue'
-import axios from 'axios'
-
-const data = ref([])
-
-const getList = async () => {
-    try {
-        const response = await axios.get('http://localhost:3000/personalHistory')
-        data.value = response.data
-    } catch (error) {
-        console.error('Error fetching data:', error)
-    }
-}
-
-onMounted(() => {
-    getList()
-})
-</script>
+  </template>
+  
+  <script setup>
+  import { defineProps } from 'vue';
+  
+  const props = defineProps({
+    data: Array
+  });
+  </script>
+  
